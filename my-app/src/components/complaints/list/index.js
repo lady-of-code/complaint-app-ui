8dddd35b3438c48f36626e8ce13757451ec3afc9
Deepak Belbase <deepak.belbase4@gmail.com>
2022-11-28 21:03:31 +0530

    Added: React Build in Index

---
+++ my-app/src/components/complaints/list/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+class List extends React.Component{
+    render(){
+        return (<React.Fragment>
+                    <h1>Hello World!</h1>
+                    <p>Hello</p>
+                </React.Fragment>)
+    }
+}
+
+export default List;
