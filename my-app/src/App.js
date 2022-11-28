f75fcd3671cbccc71a8feda83588614a695a8038
Deepak Belbase <deepak.belbase4@gmail.com>
2022-11-28 21:18:32 +0530

    Added: More Style to the Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="main-container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,7 +28,11 @@ function App() {
           </div>
         </div>
       </nav>
-      <List/>
+      <div className='container'>
+        <List/>
+        <List/>
+        <List/>
+      </div>
     </div>
   );
 }
--- my-app/src/components/complaints/list/index.js
+++ my-app/src/components/complaints/list/index.js
@@ -2,10 +2,20 @@ import * as React from "react";
 
 class List extends React.Component{
     render(){
-        return (<React.Fragment>
-                    <h1>Hello World!</h1>
-                    <p>Hello</p>
-                </React.Fragment>)
+        return (
+            <div className="row">
+            <div className="col pt-5">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Complaint No 1</h5>
+                            <h6 className="card-subtitle mb-2 text-muted">26/11/2022</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="card-link">Card link</a>
+                        </div>
+                        </div>
+                </div>
+            </div>
+        )
     }
 }
 
