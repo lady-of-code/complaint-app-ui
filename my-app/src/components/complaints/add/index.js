3164b6ae039787983b6636383c4d3f651128e4d2
Sakshi Sharma <sakshiansh2003@gmail.com>
2022-11-30 11:20:13 +0530

    added events and reset in form

--- my-app/src/components/complaints/add/index.js
+++ my-app/src/components/complaints/add/index.js
@@ -4,10 +4,31 @@ class Add extends React.Component{
     constructor(props){
         super(props)
         console.log("heyaaa good morning", this.props)
-        
+        this.onFormSubmit=this.onFormSubmit.bind(this)
+        this.onFormType=this.onFormType.bind(this)
+        this.state={name:"", email:"", complaint:""}
     }
-  
-    render()
+    // this is a method 
+  onFormSubmit(event){
+//    event.preventDefault();
+
+event.preventDefault()
+  let form=this.state;
+ console.log("submitted data =", form);
+ this.addComplaintForm.reset();
+  }
+  onFormType(event)
+    {
+    //  console.log(event.target.name);
+    //  console.log(event.target.value);
+     let form=this.state;
+     form[event.target.name]=event.target.value;
+     this.setState(form);
+    // this.state=form;
+
+
+    }  
+  render()
     {
         let {
             name
@@ -18,20 +39,21 @@ class Add extends React.Component{
             </div><div>
 
                 </div><div>
-                    <form>
-                        <div className="mb-3">
-                            <label forHtml="exampleInputEmail1" className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    <form ref={(el) => this.addComplaintForm = el} onSubmit={this.onFormSubmit}>
+                        
+                    <div className="mb-3">
+                            <label forHtml="exampleInputName" className="form-label">Name</label>
+                            <input type="text" className="form-control" id="exampleInputName" name="name1" onKeyUp={this.onFormType} />
                         </div>
                         <div className="mb-3">
-                            <label forHtml="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" />
+                            <label forHtml="exampleInputEmail" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="exampleInputEmail" name="email" onKeyUp={this.onFormType}/>
                         </div>
-                        <div className="mb-3 form-check">
-                            <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                            <label className="form-check-label" forHtml="exampleCheck1">Check me out</label>
+                        <div className="mb-3">
+                            <label forHtml="exampleInputPassword1" className="form-label">Complaints</label>
+                            <textarea className="form-control" id="exampleInputPassword1" rows="5" name="complaint" onKeyUp={this.onFormType} />
                         </div>
+        
                         <button type="submit" className="btn btn-primary">Submit</button>
                     </form></div></React.Fragment>
  
