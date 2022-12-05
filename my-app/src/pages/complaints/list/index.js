5f16d5e1c94d4940ee6bb06259090a49f73f3eb3
Sakshi Sharma <sakshisharma.cse19@chitkarauniversity.edu.in>
2022-12-05 11:59:36 +0530

    added backend support and consume list api

--- my-app/src/pages/complaints/list/index.js
+++ my-app/src/pages/complaints/list/index.js
@@ -12,8 +12,16 @@ class ListPage extends React.Component {
 
         console.log(storage);
 
-        this.state = { data: storage }
+        this.state = { data: storage, label:" " }
         this.deleteComplaints = this.deleteComplaints.bind(this)
+    }
+    componentDidMount(){
+        fetch("http://localhost:4000/api/complaints")
+        .then(res => res.json())
+        .then((res)=>{
+         console.log("respons is",res);
+         this.setState({label:res.data})
+        })
     }
     deleteComplaints(event) {
         localStorage.removeItem("complaint");
@@ -23,11 +31,12 @@ class ListPage extends React.Component {
 
     }
     render() {
-        let { data } = this.state;
+        let { data,label } = this.state;
         return (<React.Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col pt-3">
+                    <h3>{label}</h3>
                         <button type="button" className="btn btn-outline-danger float-end" onClick={this.deleteComplaints}>Delete All Data</button>
                     </div>
                 </div>
