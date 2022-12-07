81c5a37fadac54654b9d3456bd172eb1bb9abbf9
Sakshi Sharma <sakshisharma.cse19@chitkarauniversity.edu.in>
2022-12-08 00:20:42 +0530

    added icons and table in complaint list

--- src/pages/complaints/list/index.js
+++ src/pages/complaints/list/index.js
@@ -1,49 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import complaintService from "../../../services/complaints.service";
-import Card from "../../../ui-components/card";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
+
 
 class ListComplaint extends React.Component {
     constructor(props) {
         super(props);
         let storage = complaintService.list();
-        this.state = { data: storage, label:" " }
-        this.deleteComplaints = this.deleteComplaints.bind(this)
+        this.state = { data: storage, label: "Complaint List" }
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         let label = await complaintService.fetchListAPI()
-         console.log(label.data);
-        this.setState({data:label.data})
-    }
-
-    deleteComplaints(event) {
-        complaintService.deleteAll();
-        this.setState({
-            data: []
-        })
+        console.log(label.data);
+        this.setState({ data: label.data })
     }
 
     render() {
-        let { data,label } = this.state;
+        let { data, label } = this.state;
         return (<React.Fragment>
             <div className="container">
                 <div className="row">
                     <div className="col pt-3">
-                    <h3>{label}</h3>
-                        <button type="button" className="btn btn-outline-danger float-end" onClick={this.deleteComplaints}>Delete All Data</button>
+                        <h3>{label}</h3>
+                        <Link to={"/add"} className="btn btn-outline-primary float-end">Add new</Link>
                     </div>
                 </div>
             </div>
             <div className="container">
                 <div className="row">
-                    {data.map(
-                        (d, i) => {
-                            return (
-                                <div className="col-12 p-2" key={i}>
-                                    <Card title={d.name + ' - '+ d.email} subtitle={d.created_at} text={d.complaint_desc} />
-                                </div>)
-                        }
-                    )}
+                    <div className="col-12">
+                        <div className="pt-2 ">
+                            <table className="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Complaint</th>
+                                    <th scope="col">Created At</th>
+                                    <th scope="col">Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    {data.map(
+                                        (d, i) => {
+                                            return (
+                                                <tr key={i}>
+                                                    <td>{d.id}</td>
+                                                    <td>{d.name}</td>
+                                                    <td>{d.email}</td>
+                                                    <td>{d.complaint_desc}</td>
+                                                    <td>{d.created_at}</td>
+                                                    <td>
+                                                    <a href="#"><FontAwesomeIcon icon={icon({name: 'pencil', style: 'solid'})} /></a> &nbsp;
+                                                    <a href="#"><FontAwesomeIcon icon={icon({name: 'eye', style: 'solid'})} /></a> &nbsp;
+                                                    <a href="#"><FontAwesomeIcon icon={icon({name: 'trash', style: 'solid'})} /></a> 
+                                                
+                                                    </td>
+                                                </tr>
+                                            )
+                                        }
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </React.Fragment>)
--- src/ui-components/nav/index.js
+++ src/ui-components/nav/index.js
@@ -12,10 +12,7 @@ class Navbar extends React.Component {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/list">List</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/add">Add</Link>
+                <Link className="nav-link active" aria-current="page" to="/list">Complaint List</Link>
               </li>
             </ul>
           </div>
